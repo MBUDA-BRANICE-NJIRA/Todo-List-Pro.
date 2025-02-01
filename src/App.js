a241770efe9d802af1b2e7ecf4cb3ad07a7f5653
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-01 16:23:52 +0300

    Strted by piting the title in the App.js section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Best Planner</h1>
+
+      <div className="todo-box">
+        <div className="todo-input">
+          
+          <div>
+            <label htmlFor="">Title</label>
+            <input type="text" placeholder='Task'/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
