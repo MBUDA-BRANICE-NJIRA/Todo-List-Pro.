5c49e5f6124fe24768d0cd8dc4e05e41ba8d352b
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-01 19:00:35 +0300

    Styled the todo button using javascript by calling in a function and using if else statement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+
   return (
     <div className="App">
       <h1>The Best Planner</h1>
 
       <div className="todo-box">
         <div className="todo-input">
-         
           <div className="todo-items">
             <label>Title</label>
             <input type="text" placeholder="Task For the Day" />
@@ -23,19 +25,27 @@ function App() {
           </button>
         </div>
 
-
         <div className="btnArea">
-            <button>Todo</button>
-            <button>Completed</button>
-          </div>
+          <button
+            className={`secbtn ${isCompleteScreen === false && `active`}`}
+            onClick={() => setIsCompleteScreen(false)}
+          >
+            Todo
+          </button>
+          <button
+            className={`secbtn ${isCompleteScreen === true && `active`}`}
+            onClick={() => setIsCompleteScreen(true)}
+          >
+            Completed
+          </button>
+        </div>
 
-          <div className="displayArea">
-            <div className="items">
-              <h2>Task 1</h2>
-              <p> Description</p>
-            </div>
+        <div className="displayArea">
+          <div className="items">
+            <h2>Task 1</h2>
+            <p> Description</p>
           </div>
-
+        </div>
       </div>
     </div>
   );
