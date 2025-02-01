9cae558883b3f6fcda9023b0b278a390fd0a9e5f
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-01 16:31:06 +0300

    Created the button section

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ function App() {
       <div className="todo-box">
         <div className="todo-input">
           
-          <div>
+          <div className="todo-items">
             <label htmlFor="">Title</label>
-            <input type="text" placeholder='Task'/>
+            <input type="text" placeholder='Task For the Day' />
           </div>
+
+          <div className="todo-items">
+            <label htmlFor="">Descriiption</label>
+            <input type="text" placeholder='Task For the Description' />
+          </div>
+
+          <button className="primary-button" type='button'>Add</button>
+         
         </div>
       </div>
     </div>
