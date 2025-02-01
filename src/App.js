24641293af4561605c2c6aed8411daf54442de10
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-01 17:15:21 +0300

    Styled the input and gave it a focus display using css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
@@ -7,39 +7,38 @@ function App() {
 
       <div className="todo-box">
         <div className="todo-input">
-          
-          <div className="todo-items">
-            <label htmlFor="">Title</label>
-            <input type="text" placeholder='Task For the Day' />
-          </div>
-
-          <div className="todo-items">
-            <label htmlFor="">Descriiption</label>
-            <input type="text" placeholder='Task For the Description' />
-          </div>
-
-          <button className="primary-button" type='button'>Add</button>
          
+          <div className="todo-items">
+            <label>Title</label>
+            <input type="text" placeholder="Task For the Day" />
+          </div>
+
+          <div className="todo-items">
+            <label>Descriiption</label>
+            <input type="text" placeholder="Task For the Description" />
+          </div>
+
+          <button className="primary-button" type="button">
+            Add
+          </button>
         </div>
+
+
+        <div className="btnArea">
+            <button>Todo</button>
+            <button>Completed</button>
+          </div>
+
+          <div className="displayArea">
+            <div className="items">
+              <h2>Task 1</h2>
+              <p> Description</p>
+            </div>
+          </div>
+
       </div>
-
-      <div className="btnArea">
-        <button>Todo</button>
-        <button>Completed</button>
-      </div>
-
-      <div className="displayArea">
-        <div className="items">
-          <h2>Task1</h2>
-          <p> Description</p>
-
-        </div>
-      </div>
-
     </div>
   );
 }
 
 export default App;
-
-
